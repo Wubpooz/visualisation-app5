29399222b9daa0134da4e2c94fc8f22481c8656e
emma <emma.degot@gmail.com>
2026-04-03 08:45:58 +0200

    Refactor code structure for improved readability and maintainability

--- mapping.py
+++ mapping.py
@@ -1,1 +1,44 @@
 # freq: A = Annual (discard since always same value)
+
+#unit :
+
+# "TIME_SP":
+#     kind: unit_code
+#     level: standard
+#     urn: urn:sdmx:org.sdmx.infomodel.codelist.Code=ESTAT:UNIT(64.0).TIME_SP
+#     name:
+#       en: "Time spent (hh:mm)"
+#       de: "Vergehende Zeit (hh:mm)"
+#       fr: "Temps passé (hh:mm)"
+#     is_aggregate: false
+#     aggregate_components: []
+#     aggregate_exclusions: []
+#     explan_label:
+#       en: "Code not associated with the concept of the code list, but following Eurostat Standard Code List Guidelines"
+# "PTP_TIME":
+#     kind: unit_code
+#     level: standard
+#     urn: urn:sdmx:org.sdmx.infomodel.codelist.Code=ESTAT:UNIT(64.0).PTP_TIME
+#     name:
+#       en: "Participation time (hh:mm)"
+#       de: "Teilnahmezeit (hh:mm)"
+#       fr: "Temps de participation (hh:mm)"
+#     is_aggregate: false
+#     aggregate_components: []
+#     aggregate_exclusions: []
+#     explan_label:
+#       en: "Code not associated with the concept of the code list, but following Eurostat Standard Code List Guidelines"
+# "PTP_RT":
+#     kind: unit_code
+#     level: standard
+#     urn: urn:sdmx:org.sdmx.infomodel.codelist.Code=ESTAT:UNIT(64.0).PTP_RT
+#     name:
+#       en: "Participation rate (%)"
+#       de: "Teilnahmerate (%)"
+#       fr: "Taux de participation (%)"
+#     is_aggregate: false
+#     aggregate_components: []
+#     aggregate_exclusions: []
+#     explan_label:
+#       en: "Code not associated with the concept of the code list, but following Eurostat Standard Code List Guidelines"
+  
